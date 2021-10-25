5613936c021c71f31a778be2a8dda597ad6cb84c
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-10-25 14:15:05 +0530

    Refactored content of the code

--- datastructureproblems/FindYourNumber.java
+++ datastructureproblems/FindYourNumber.java
@@ -3,70 +3,44 @@ package datastructureproblems;
 import java.util.Scanner;
 
 public class FindYourNumber {
-	     Scanner scanner = new Scanner(System.in);
-		int[] numberArray;	//	array of numbers of given range
-		int count = 0;	
-		int  ans;	//	the number in mind
-		int N;	//	range	
-		double  maxCount = 0;	//	number of times question will be asked
-		
-		public static void main(String[] args) 
-		{
-			FindYourNumber findYourNumber = new FindYourNumber();
-			findYourNumber.start();
-		}
+	public static void main(String[] args) {
+		FindYourNumber findNumber = new FindYourNumber();
+		// TODO Auto-generated method stub
+		int n = Integer.parseInt("10");
+		int range = (int) Math.pow(2, n);
+		System.out.println("Range is 0 to " + range);
+		findNumber.yourNumber(range);
 
-		//takes range from user
-		
-		void start() 
-		{
-			System.out.print("Enter limit N: ");
-			N = scanner.nextInt();
-
-			numberArray = new int[N];	//	array initialization
-			
-			//	fills the array 
-			
-			for(int i = 0; i < N; i++) {
-				numberArray[i] = i;
-			}
-			
-			//	calculates maximum count of questions
-			maxCount = (Math.log(N) / Math.log(2));
-			System.out.println("Number of tries required to guess: " + ((int)maxCount + 1));
-			binarySearch(0, N-1);
-		}
-		void binarySearch(int first, int last) 
-		{
-			if(count < maxCount) 
-			{
-				int	middle = (first + last) / 2;
-				count++;
-				System.out.println("Is number between " + first  + " and " + middle + "?");
-				String temp = scanner.next();
-				if(temp.equals("y")) {
-					change(middle+1 , last);
-					binarySearch(first , middle);				
-				}
-				else if(temp.equals("n")){
-					change(first , middle);
-					binarySearch(middle + 1, last);
-				}
-			}
-			else {
-				for(int i : numberArray) {
-					if(i != -1) {
-						System.out.println("The number is: " + i);
-					}
-				}
-			}
-		}
-		
+	}
 	
-		void change(int first , int last) {
-			for(int i = first; i <= last; i++) {
-				numberArray[i] = -1;
+	public  void yourNumber(int range) {
+		Scanner scanner = new Scanner(System.in);
+		int li = 0;
+		int hi = range - 1;
+		int mi = (li + hi) / 2;
+
+		System.out.println();
+		System.out.println("Think of a number in your mind between 0 to " + (range - 1));
+
+		while (li < hi) {
+			System.out.println("if your number is between " + li + " to " + mi + " Press 1 ");
+			System.out.println("if your number is between " + (mi + 1) + " to " + hi + " Press 2");
+
+			int uservalue = scanner.nextInt();
+			if (uservalue == 1) {
+
+				hi = mi;
+				mi = (li + hi) / 2;
+
+			} else if (uservalue == 2) {
+				li = mi + 1;
+				mi = (li + hi) / 2;
+			} else {
+				System.out.println("Wrong input ");
 			}
 		}
-}
 
+		System.out.println("Your number is :" + mi);
+		scanner.close();
+	}
+}
